c5169007d2b157ccf5f997919a498166285fa4d8
Sri-Charan-0516 <sricharan824@gmail.com>
2025-05-27 12:49:42 +0530

    27/05/2025

--- src/main/java/com/realestate/main/redisConfiguration/RedisConfig.java
+++ src/main/java/com/realestate/main/redisConfiguration/RedisConfig.java
@@ -2,6 +2,7 @@ package com.realestate.main.redisConfiguration;
 
 import java.net.URI;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,23 +10,33 @@ import org.springframework.data.redis.connection.RedisPassword;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.serializer.StringRedisSerializer;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 @EnableCaching
 public class RedisConfig {
 
+	@Value("${REDISCLOUD_URL}")
+	private String redisUrl;
+
 	@Bean
 	public LettuceConnectionFactory redisConnectionFactory() {
-		URI redisUri = URI.create(System.getenv("REDISCLOUD_URL"));
-		RedisStandaloneConfiguration config = new RedisStandaloneConfiguration();
-		config.setHostName(redisUri.getHost());
-		config.setPort(redisUri.getPort());
-		String[] userInfo = redisUri.getUserInfo().split(":", 2);
-		config.setPassword(RedisPassword.of(userInfo[1]));
-		return new LettuceConnectionFactory(config);
+	    if (redisUrl == null || redisUrl.isEmpty()) {
+	        throw new IllegalArgumentException("REDISCLOUD_URL is not set or empty");
+	    }
+
+	    URI redisUri = URI.create(redisUrl);
+	    RedisStandaloneConfiguration config = new RedisStandaloneConfiguration();
+	    config.setHostName(redisUri.getHost());
+	    config.setPort(redisUri.getPort());
+
+	    String[] userInfo = redisUri.getUserInfo().split(":", 2);
+	    config.setPassword(RedisPassword.of(userInfo[1]));
+
+	    return new LettuceConnectionFactory(config);
 	}
+
 
 	@Bean
 	public RedisTemplate<String, Object> redisTemplate() {
