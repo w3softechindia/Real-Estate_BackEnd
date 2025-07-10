05595630247bcc7f760ec83f4ae488a18f0adb14
purna <p.purnababu@gmail.com>
2025-07-10 12:05:23 +0530

    updtaed

---
+++ src/main/java/com/realestate/main/repository/Postrepository.java
@@ -0,0 +1,24 @@
+package com.realestate.main.repository;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import com.realestate.main.entity.Agency;
+import com.realestate.main.entity.Post;
+
+import io.lettuce.core.dynamic.annotation.Param;
+
+@Repository
+public interface Postrepository extends JpaRepository<Post, Long> {
+
+	List<Post> findByAgencyEmail(String email);
+
+	
+
+	
+
+}
