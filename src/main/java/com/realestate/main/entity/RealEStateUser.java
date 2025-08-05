759e88766463f3e6c867ff112b01223361b6bcd9
Sri-Charan-0516 <sricharan824@gmail.com>
2025-08-05 09:01:28 +0530

    05/08/2025

--- src/main/java/com/realestate/main/entity/RealEStateUser.java
+++ src/main/java/com/realestate/main/entity/RealEStateUser.java
@@ -48,8 +48,6 @@ public class RealEStateUser implements UserDetails {
 	
 	private int otp;
 
-	private int otp;
-
 	@ManyToMany(fetch = FetchType.EAGER, cascade = { CascadeType.PERSIST, CascadeType.MERGE })
 	@JoinTable(name = "User_Roles", joinColumns = { @JoinColumn(name = "User_Id") }, inverseJoinColumns = {
 			@JoinColumn(name = "Role_Name") })
