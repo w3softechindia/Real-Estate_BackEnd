33f8bba9b79f98bd123a1194b339f59e5d45487e
purna <p.purnababu@gmail.com>
2025-08-15 11:49:09 +0530

    updated

---
+++ src/main/java/com/realestate/main/repository/ReviewsRepository.java
@@ -0,0 +1,13 @@
+package com.realestate.main.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.realestate.main.entity.Reviews;
+
+@Repository
+public interface ReviewsRepository extends JpaRepository<Reviews, Long> {
+	List<Reviews> findByAgentEmail(String email);
+}
